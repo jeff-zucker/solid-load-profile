fc0c0d76c5f7c6a6df3c80804c87f68599e23c68
jeff-zucker <dubzed@gmail.com>
2026-01-26 17:13:31 -0800

    redo solidos imports

--- loadProfile-local.js
+++
@@ -1,4 +0,0 @@
-import * as $rdf from "./node_modules/rdflib/dist/rdflib.min.js";
-import {authn,store} from "./node_modules/solid-logic/dist/solid-logic.min.js";
-import {ns} from "./node_modules/solid-ui/dist/solid-ui.min.js";
-window.UI = { store, authn, ns, rdf:$rdf };
